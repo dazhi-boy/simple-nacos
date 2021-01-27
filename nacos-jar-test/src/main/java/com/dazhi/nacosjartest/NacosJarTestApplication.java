a0a5c17fb9e01de1cc738dca1d0980e88321eb4e
zhiyong.guo <zhiyong.guo@bioknow.net>
2021-01-27 19:39:35 +0800

    模拟一个提供服务的系统

---
+++ nacos-jar-test/src/main/java/com/dazhi/nacosjartest/NacosJarTestApplication.java
@@ -0,0 +1,13 @@
+package com.dazhi.nacosjartest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosJarTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosJarTestApplication.class, args);
+    }
+
+}
---
+++ nacos-jar-test/src/test/java/com/dazhi/nacosjartest/NacosJarTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dazhi.nacosjartest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NacosJarTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
