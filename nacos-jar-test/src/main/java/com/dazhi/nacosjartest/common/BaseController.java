af98b8c9334a1af0ac0c05fe80fc3dc472d0d183
zhiyong.guo <zhiyong.guo@bioknow.net>
2021-03-04 18:54:44 +0800

    模拟nacos配置管理

--- nacos-jar-test/src/main/java/com/dazhi/nacosjartest/common/BaseController.java
+++ nacos-jar-test/src/main/java/com/dazhi/nacosjartest/common/BaseController.java
@@ -1,5 +1,6 @@
 package com.dazhi.nacosjartest.common;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("base")
 public class BaseController {
 
+    @Value("${useLocalCache:false}")
+    private boolean useLocalCache;
+
     @GetMapping
     public String sayHello(){
         return "hello";
+    }
+
+    @RequestMapping("/get")
+    public boolean get() {
+        return useLocalCache;
     }
 }
